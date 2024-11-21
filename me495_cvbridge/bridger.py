05803e61acd496f37b2508e689a4930c2cec3ee8
Matthew Elwin <elwin@northwestern.edu>
2024-11-20 20:50:43 -0600

    Passes flake8

--- me495_cvbridge/bridger.py
+++ me495_cvbridge/bridger.py
@@ -1,12 +1,14 @@
+import cv2 as cv
+
+from cv_bridge import CvBridge
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-import cv2 as cv
-from cv_bridge import CvBridge
+
 
 class BridgeNode(Node):
     """
-    Node that reads images and draws on them using open cv
+    Node that reads images and draws on them using open cv.
 
     Publishers
     ----------
@@ -16,6 +18,7 @@ class BridgeNode(Node):
     -----------
     image (sensor_msgs/msg/Image): The image on which to do the processing
     """
+
     def __init__(self):
         super().__init__('bridge')
         self.bridge = CvBridge()
@@ -23,11 +26,12 @@ class BridgeNode(Node):
         self.pub = self.create_publisher(Image, 'new_image', 10)
 
     def opencv_process(self, image):
-        """Draw a circle on the subscribed image and republish it to new_image"""
+        """Draw a circle on the subscribed image and republish it to new_image."""
         cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
-        cv.circle(cv_image, (200, 200), 40, (0,0,255), -1)
+        cv.circle(cv_image, (200, 200), 40, (0, 0, 255), -1)
         new_msg = self.bridge.cv2_to_imgmsg(cv_image, encoding='bgr8')
         self.pub.publish(new_msg)
+
 
 def main():
     rclpy.init()
